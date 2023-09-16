7d942a70e7695b8771a76df364400c0be5fd476e
Tyler Gaw <tagaw@ucsc.edu>
2023-09-15 18:26:52 -0700

    test update

--- src/wibble/warble.py
+++
@@ -1,6 +0,0 @@
-class Warble:
-    def __init__(self):
-        pass
-
-    def doIt(self):
-        print("Warbling")
--- src/wibble/wobble.py
+++
@@ -1,9 +0,0 @@
-#! python3
-
-from wibble.warble import Warble
-
-
-def wobble():
-    print("Wobbling")
-    other = Warble()
-    other.doIt()
